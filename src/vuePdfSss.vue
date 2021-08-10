c171ad3871bdb1f0a5dc95f6553f959a53dfddc0
zhenyutsai <zhenyutsai@foxmail.com>
2021-08-10 14:52:10 +0800

    vue-pdf-up初始提交

---
+++ src/vuePdfSss.vue
@@ -0,0 +1,23 @@
+<style src="./annotationLayer.css"></style>
+<script>
+
+	import componentFactory from './componentFactory'
+	import pdfjsWrapper from './pdfjsWrapper'
+
+	var PDFJS = require('pdfjs-dist/build/pdf.js');
+
+	if ( process.env.VUE_ENV !== 'server' ) {
+
+		if ( typeof window !== 'undefined' && 'Worker' in window ) {
+
+			var PdfjsWorker = require('worker-loader!pdfjs-dist/build/pdf.worker.js');
+			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+		}
+	}
+
+	var component = componentFactory(pdfjsWrapper(PDFJS));
+	component.PDFJS = PDFJS;
+
+	export default component;
+
+</script>
