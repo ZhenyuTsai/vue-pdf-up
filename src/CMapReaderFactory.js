8bbb451063235f5433874be5ed64d11c0586ab53
zhenyutsai <zhenyutsai@foxmail.com>
2021-08-10 15:55:04 +0800

    修改2.0

--- src/CMapReaderFactory.js
+++ src/CMapReaderFactory.js
@@ -1,4 +1,4 @@
-import { CMapCompressionType } from 'pdfjs-dist/build/pdf.js'
+import { CMapCompressionType } from '../pdfjs-dist/es5/build/pdf.js'
 
 // see https://github.com/mozilla/pdf.js/blob/628e70fbb5dea3b9066aa5c34cca70aaafef8db2/src/display/dom_utils.js#L64
 
@@ -6,7 +6,7 @@ export default function() {
 
 	this.fetch = function(query) {
 
-		return import('./buffer-loader!pdfjs-dist/cmaps/'+query.name+'.bcmap' /* webpackChunkName: "noprefetch-[request]" */)
+		return import('./buffer-loader!../pdfjs-dist/cmaps/'+query.name+'.bcmap' /* webpackChunkName: "noprefetch-[request]" */)
 		.then(function(bcmap) {
 
 			return {
